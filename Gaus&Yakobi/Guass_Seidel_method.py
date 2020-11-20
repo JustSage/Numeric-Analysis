e4d7aa2fc7b3d2a0f6ad924797287f87d4c0e654
RabbitJazz <sagebaram@gmail.com>
2020-11-20 02:16:25 +0200

    " added more comments and logger"

---
+++ Gaus&Yakobi/Guass_Seidel_method.py
@@ -0,0 +1,68 @@
+import logging
+import sys
+
+a_logger = logging.getLogger()
+a_logger.setLevel(logging.DEBUG)
+
+output_file_handler = logging.FileHandler("output.log")
+stdout_handler = logging.StreamHandler(sys.stdout)
+
+a_logger.addHandler(output_file_handler)
+a_logger.addHandler(stdout_handler)
+
+
+def gaus_seidel(matrix, result, x_variables):
+    """
+        Gaus Seidel algorithm, is an iterative method used to solve a system of
+        linear equations. Can be applied to any matrix with non-zero elements
+        on the diagonals, convergence is ony guarenteed if the matrix is either
+        strictly diagonally dominant (D), or symmetric and positive definite.
+
+        Analytic Example:
+
+        4X + 2Y = 2          ===>  Xr+1 = (2-2*Y-0*Z)/4
+        2X + 10Y + 4Z = 6    ===>  Yr+1 = (6-2*X - 4*Z)/10
+        4Y + 5Z = 5          ===>  Zr+1 = (5-0*X-4*Y)/5
+
+        0   1   2
+       ___________
+    0 | 4   2   0 |
+      |           |
+    1 | 2  10   4 |
+      |           |
+    2 | 0   4   5 |
+      |___________|
+
+        matrix - assuming it's an N*N matrix (A)
+        result - the result column matrix (b)
+        x_variables - the variable column matrix (x)
+    """
+    size = len(matrix)
+    for col in range(0, size):
+        holder = result[col]
+        for row in range(0, size):
+            if col != row:  # if not diagonal index
+                holder -= matrix[col][row] * x_variables[row]
+                # calculating variables x**(val=0,1,2,3,...)
+        x_variables[col] = holder / matrix[col][col]
+        # divide by dominant diagonal
+    return x_variables  # returns the updated values.
+
+
+# Data Input (from class)
+
+max_iterations = int(input("Enter maximum iterations: "))
+x = [0, 0, 0]
+result = [2, 6, 5]
+matrix = [[4, 2, 0], [2, 10, 4], [0, 4, 5]]
+count = 0
+
+print((f"\t Xn+1\t\t Yn+1\t\t Zn+1"))  # test data got 3 variables to iterate
+x_init = list(map(lambda y: "{:.6f}".format(y), x))  # maps 6 decimals after .
+print(f"{count}\t{x_init}")
+
+for i in range(0, max_iterations):
+    x = gaus_seidel(matrix, result, x)  # call gaus_seidel repeatedly
+    x_formatted = list(map(lambda y: "{:.6f}".format(y), x))
+    a_logger.debug((f"{count+1}\t{x_formatted}"))  # print count and formatted values.
+    count += 1
